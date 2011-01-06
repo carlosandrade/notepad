6cc4d667581373f26967d614b06a67df009dc981
Carlos Andrade <carlosviansi@gmail.com>
2011-01-06 15:05:01 -0300

    Attempt to encapsulate the optional feature 'About'
    -Created a new class AboutActions to distinguish actions related to about feature
    - Removed About related actions from the Actions class

---
+++ AboutActions.java
@@ -0,0 +1,22 @@
+
+import javax.swing.JOptionPane;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author carlos
+ */
+public class AboutActions {
+
+  	/**
+	 *@see ABOUT.JAVA
+	 *it's a Message Dialog to show the information about this program
+	 */
+	public void about(){
+		JOptionPane.showMessageDialog(null, new About(),"About Notepad",JOptionPane.PLAIN_MESSAGE);
+	}
+}
