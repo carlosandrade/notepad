3ee93c93789320dc7b6d693166750abb8d55b883
Carlos Andrade <carlosviansi@gmail.com>
2011-01-06 17:28:25 -0300

    Packing About Feature
    Packed the About feature. Adding this feature now requires Notepad to import the about package. Excluding this package does not affect the main application unless the method to add this feature is used on Notepad class, which decreases code amount when this feature is not used.
    	- Java image had some issues on the About class, so I added comments. Will look into this later on.

---
+++ about/AboutFeature.java
@@ -0,0 +1,42 @@
+package about;
+
+import Notepad.*;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.ImageIcon;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * A class in charge of adding the About feature to the Notepad
+ * @author carlos
+ */
+public class AboutFeature {
+
+ static public AboutActions aboutActions = new AboutActions();
+ static JMenuItem aboutMenuItem = aboutMenuItem = new JMenuItem("About Notepad");
+ 
+    	static public void add() {
+		JMenu helpMenu   = new JMenu("Help");
+		helpMenu.setMnemonic('h');
+
+                /*About Feature */
+	  
+		aboutMenuItem.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent ae){
+				aboutActions.about();
+			}
+		});
+		helpMenu.add(aboutMenuItem);
+
+                /* End of About Feature */
+
+		Notepad.getNotepadMenuBar().add(helpMenu);
+	}
+}
